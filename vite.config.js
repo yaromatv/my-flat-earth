ec69984839bf1ad158a3b2c0750381f8d9eb0729
Yaroslav Matvieiev <yaromatv@gmail.com>
2024-06-23 11:29:21 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig(({ mode }) => {
     const { GOOGLE_MAPS_API_KEY = "" } = loadEnv(mode, process.cwd(), "");
 
     return {
+        base: "/my-flat-earth/",
         define: {
             "process.env.GOOGLE_MAPS_API_KEY":
                 JSON.stringify(GOOGLE_MAPS_API_KEY),
@@ -12,6 +13,11 @@ export default defineConfig(({ mode }) => {
             alias: {
                 "@vis.gl/react-google-maps/examples.js":
                     "https://visgl.github.io/react-google-maps/scripts/examples.js",
+            },
+        },
+        server: {
+            mimeTypes: {
+                "application/javascript": ["js", "mjs"],
             },
         },
     };
